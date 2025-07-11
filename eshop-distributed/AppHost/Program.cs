8e3f9099dc862a4cd9eed92836d1b60e147ef5bd
gururajkl <gururajragavendra7@gmail.com>
2025-07-11 20:22:42 +0530

    Create Catalog microservice and add that project to aspire AppHost - aspire orchestration

--- eshop-distributed/AppHost/Program.cs
+++ eshop-distributed/AppHost/Program.cs
@@ -1,4 +1,9 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+// Add projects and cloud native backing services.
+builder.AddProject<Catalog>("catalog");
 
 // Add projects and cloud native backing services.
 
