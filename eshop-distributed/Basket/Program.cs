82d27c8b95f240e9af50b5b7d2bc470376c8fbbc
gururajkl <gururajragavendra7@gmail.com>
2025-07-12 18:12:14 +0530

    Add Basket client integration

--- eshop-distributed/Basket/Program.cs
+++ eshop-distributed/Basket/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.AddServiceDefaults();
 
+// Instruct basket ms to connect to the redis cache.
+builder.AddRedisDistributedCache("cache");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
