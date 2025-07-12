c4e28784e1f1b0fc2a7e6cc34c36247b1dbf89d2
gururajkl <gururajragavendra7@gmail.com>
2025-07-12 17:58:46 +0530

    Create integration hosting for basket microservice

--- eshop-distributed/AppHost/Program.cs
+++ eshop-distributed/AppHost/Program.cs
@@ -8,8 +8,11 @@ var postgres = builder.AddPostgres("postgres").WithPgAdmin().WithDataVolume().Wi
 // Create catalogdb using postgres.
 var catalogdb = postgres.AddDatabase("catalogdb"); // Gives connectionInfo to the MS.
 
-// Catalog project depends on catalogdb.
+// Redis as backing service and using this in basket ms.
+var cache = builder.AddRedis("cache").WithRedisInsight().WithDataVolume().WithLifetime(ContainerLifetime.Persistent);
+
+// Projects and their references.
 builder.AddProject<Catalog>("catalog").WithReference(catalogdb).WaitFor(catalogdb);
-builder.AddProject<Basket>("basket");
+builder.AddProject<Basket>("basket").WithReference(cache).WaitFor(cache);
 
 builder.Build().Run();
