a2a54bf7428104423c47ad32ca8d1373efd4b311
gururajkl <gururajragavendra7@gmail.com>
2025-07-12 17:52:17 +0530

    Create entities of Basket microservice

---
+++ eshop-distributed/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.Models;
+
+/// <summary>
+/// Entity representing a shopping cart for a user.
+/// </summary>
+public class ShoppingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(t => t.Price * t.Quantity);
+}
---
+++ eshop-distributed/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Basket.Models;
+
+/// <summary>
+/// Entity representing an item in a shopping cart.
+/// </summary>
+public class ShoppingCartItem
+{
+    public int ProductId { get; set; } = default!;
+    public string Color { get; set; } = default!;
+    public int Quantity { get; set; } = default!;
+
+    // These comes from the Catalog module.
+    public decimal Price { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
+}
