141b837c479ae97df4de2f65ad64484029c0b06c
gururajkl <gururajragavendra7@gmail.com>
2025-07-12 14:23:44 +0530

    Implement ProductService.cs
    - Included GetAllProducts
    - Included GetProductById
    - Included Update and Delete product methods.

---
+++ eshop-distributed/Catalog/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿namespace Catalog.Services;
+
+public class ProductService(ProductDbContext dbContext)
+{
+    /// <summary>
+    /// Gets all products from the database.
+    /// </summary>
+    /// <returns>All products.</returns>
+    public async Task<IEnumerable<Product>> GetAllProductsAsync()
+    {
+        return await dbContext.Products.ToListAsync();
+    }
+
+    /// <summary>
+    /// Gets a product by its ID from the database.
+    /// </summary>
+    /// <param name="id">Id of the product.</param>
+    /// <returns>Nullable product from the database.</returns>
+    public async Task<Product?> GetProductById(int id)
+    {
+        return await dbContext.Products.FindAsync(id);
+    }
+
+    /// <summary>
+    /// Creates a new product in the database.
+    /// </summary>
+    /// <param name="product">Product entity to add to the database.</param>
+    public async Task CreateProductAsync(Product product)
+    {
+        dbContext.Products.Add(product);
+        await dbContext.SaveChangesAsync();
+    }
+
+    /// <summary>
+    /// Updates an existing product in the database.
+    /// </summary>
+    /// <param name="updatedProduct">Old product.</param>
+    /// <param name="inputProduct">New product.</param>
+    public async Task UpdateProductAsync(Product updatedProduct, Product inputProduct)
+    {
+        updatedProduct.Name = inputProduct.Name;
+        updatedProduct.Description = inputProduct.Description;
+        updatedProduct.ImageUrl = inputProduct.ImageUrl;
+        updatedProduct.Price = inputProduct.Price;
+
+        dbContext.Products.Update(updatedProduct);
+        await dbContext.SaveChangesAsync();
+    }
+
+    /// <summary>
+    /// Deletes a product from the database.
+    /// </summary>
+    /// <param name="product">Product to delete.</param>
+    public async Task DeleteProductAsync(Product product)
+    {
+        dbContext.Products.Remove(product);
+        await dbContext.SaveChangesAsync();
+    }
+}
