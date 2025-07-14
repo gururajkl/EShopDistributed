a80f841c1e2c77c422d03595c54d3687c4a10259
gururajkl <gururajragavendra7@gmail.com>
2025-07-14 19:56:29 +0530

    Create API client in frontend

---
+++ eshop-distributed/WebApp/ApiClients/CatalogApiClient.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Models;
+
+namespace WebApp.ApiClients;
+
+public class CatalogApiClient(HttpClient client)
+{
+    public async Task<List<Product>> GetProducts()
+    {
+        var response = await client.GetFromJsonAsync<List<Product>>("/products");
+        return response!;
+    }
+
+    public async Task<Product> GetProductById(int id)
+    {
+        var response = await client.GetFromJsonAsync<Product>($"/products/{id}");
+        return response!;
+    }
+}
