a730e8e4b587bd7f057ae849b3deab71ac59d599
gururajkl <gururajragavendra7@gmail.com>
2025-07-19 18:32:37 +0530

    Ollama host integration

--- eshop-distributed/AppHost/Program.cs
+++ eshop-distributed/AppHost/Program.cs
@@ -15,12 +15,18 @@ var rabbitMq = builder.AddRabbitMQ("rabbitmq").WithManagementPlugin().WithDataVo
 
 var keycloak = builder.AddKeycloak("keycloak", 8080).WithDataVolume().WithLifetime(ContainerLifetime.Persistent);
 
+// Add Ollama as a backing service for AI capabilities.
+var ollama = builder.AddOllama("ollama", 11434).WithDataVolume().WithLifetime(ContainerLifetime.Persistent).WithOpenWebUI();
+
+var llama = ollama.AddModel("llama3.2");
+
 // Projects and their references.
 var catalog = builder.AddProject<Catalog>("catalog").WithReference(catalogdb).WithReference(rabbitMq)
     .WaitFor(catalogdb).WaitFor(rabbitMq);
+
 var basket = builder.AddProject<Basket>("basket").WithReference(cache).WithReference(catalog).WithReference(rabbitMq)
-    .WithReference(keycloak)
-    .WaitFor(cache).WaitFor(rabbitMq).WaitFor(keycloak);
+    .WithReference(keycloak).WithReference(llama)
+    .WaitFor(cache).WaitFor(rabbitMq).WaitFor(keycloak).WaitFor(llama);
 
 builder.AddProject<WebApp>("webapp").WithExternalHttpEndpoints()
     .WithReference(catalog)
